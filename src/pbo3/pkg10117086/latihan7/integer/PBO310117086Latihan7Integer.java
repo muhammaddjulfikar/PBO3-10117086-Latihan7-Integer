d84a03bf19ea725dcdb10eb72c49f1be2f3a53fd
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:20:34 -0700

    Integer

---
+++ src/pbo3/pkg10117086/latihan7/integer/PBO310117086Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan7.integer;
+
+/**
+ *
+ * @author Lenovo
+ *  Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte\t: "+ b );
+        System.out.println("short\t:" + s );
+        System.out.println("int\t: " + i );
+        System.out.println("long\t:" + l );
+    }
+    
+}
